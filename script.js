1da0d4afdce5ba8e6d47feaaff33099f4aedd8f4
Printice Lawton <printicelaw10@gmail.com>
2021-03-20 19:39:40 -0400

    Commit #8, going in a new direction

---
+++ script.js
@@ -0,0 +1,6 @@
+//This is a Javascript timed quiz to test your knowledge 
+//You will have 15 seconds to answer each question
+//Time begins with you click the Start Button
+//If you answer an question incorrectly time is subtracted from the clock
+//The game is over when all questions are answered or the timer reaches 0
+//When the game is over you can save your initials and your score
